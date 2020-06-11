8afc062a3f2d443d5bcceb36dff0a14e4b64e6a8
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-11 06:49:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,32 @@
 class StudentsController < ApplicationController
   get '/students' do
     @students = Student.all
+  end
 
+  get '/students/new' do
+    
+  end
+
+  post '/students' do
+    
+  end
+
+  get '/students/:id' do
+    
+  end
+
+  get '/students/:id/:edit' do
+  
+  end
+
+  patch '/students/:id' do
+    
+  end
+
+  delete '/newsletters/:id' do
+    
+  end
+
+
+  
 end
